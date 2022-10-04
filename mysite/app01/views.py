6678c0e8269dedd18d96ec1513d3451d38bc5f71
徐俊强 <xujunqiang_0607@126.com>
2022-10-04 22:28:45 +0800

    1-8

--- mysite/app01/views.py
+++ mysite/app01/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('欢迎使用')
+
+def user_list(request):
+    return render(request, 'user_list.html')
+
+def user_add(request):
+    return render(request, 'user_add.html')
