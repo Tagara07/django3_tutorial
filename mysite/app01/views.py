d1abf00d67e3352e2c7681f68668b83796a1bb08
徐俊强 <xujunqiang_0607@126.com>
2022-10-06 19:37:53 +0800

    haha

--- mysite/app01/views.py
+++ mysite/app01/views.py
@@ -1,5 +1,7 @@
+from email import header
 from urllib import request
 from django.shortcuts import render, HttpResponse
+import requests
 
 # Create your views here.
 
@@ -13,4 +15,26 @@ def user_add(request):
     return render(request, 'user_add.html')
 
 def tpl(request):
-    return render(request, 'tpl.html')
+    name = '韩朝'
+    roles = ['管理员', 'CEO', '保安']
+    user_info = {'name':'郭志', 'salary':10000, 'role': 'CTO'}
+
+    date_list = [
+        {'name':'郭志', 'salary':10000, 'role': 'CTO'},
+        {'name':'卢辉', 'salary':10000, 'role': 'CTO'},
+        {'name':'召见先', 'salary':10000, 'role': 'CTO'}    
+    ]
+
+    return render(request, 'tpl.html', {
+                                        "n1": name, 
+                                        'n2': roles, 
+                                        'n3': user_info,
+                                        'n4': date_list
+                                        })
+
+def news(request):
+    # headers = {'User-Agent':'这里放User-Agent'}
+    res = requests.get('http://www.chinaunicom.com.cn/api/article/NewsByIndex/2/2017/10/news')
+    data_list = res.json()
+    print(data_list)
+    return render(request, 'news.html', {'news_list': data_list})
