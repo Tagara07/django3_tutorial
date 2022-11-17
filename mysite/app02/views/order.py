eef6bcef21120ad18a0ac4ddfc10b3985735783f
徐俊强 <xujunqiang_0607@126.com>
2022-11-17 22:59:36 +0800

    20221117

--- mysite/app02/views/order.py
+++ mysite/app02/views/order.py
@@ -12,7 +12,7 @@ class OrderModelForm(BootStrapModelForm):
     class Meta:
         model = models.Order
         # fields = '__all__'
-        exclude = ['oid']
+        exclude = ['oid', 'admin']
 
 def order_list(request):
     form = OrderModelForm()
@@ -24,8 +24,13 @@ def order_add(request):
     form = OrderModelForm(data=request.POST)
 
     if form.is_valid():
-        # 随机生成oid
+        # 订单号：额外增加一些不是用户输入的值（自己计算出来）
         form.instance.oid = datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(1000, 9999))
+        
+        # 固定设置管理员ID
+        form.instance.admin_id = request.session['info']['id']
+
+        # 保存到数据库中
         form.save()
         return JsonResponse({"status": True})
 
